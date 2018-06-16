46e0d2bdb0e789055ec69c3a1f46155d7a142ab9
Magesh <magesh@Searcher.local>
2018-06-16 03:04:34 -0500

    First Commit

---
+++ src/main/java/com/myretailapp/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.myretailapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+    @RequestMapping(value = "/api", method = RequestMethod.GET)
+    @ResponseBody
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
