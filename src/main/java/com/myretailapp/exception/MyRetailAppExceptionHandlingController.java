a7a7332e774021db661972fdab763be845a4d2b5
Magesh <mageshb@live.com>
2018-06-18 22:30:25 -0500

    Handle Timeout exception

--- src/main/java/com/myretailapp/exception/MyRetailAppExceptionHandlingController.java
+++ src/main/java/com/myretailapp/exception/MyRetailAppExceptionHandlingController.java
@@ -9,12 +9,20 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class MyRetailAppExceptionHandlingController {
 
     @ExceptionHandler(UnExpectedException.class)
-    public ResponseEntity<MyRetailAppExceptionResponse> timeoutException(UnExpectedException ex) {
+    public ResponseEntity<MyRetailAppExceptionResponse> unExpectedExceptionHandler(UnExpectedException ex) {
         MyRetailAppExceptionResponse response = new MyRetailAppExceptionResponse();
         response.setErrorCode("INTERNAL_ERROR");
         response.setErrorMessage(ex.getMessage());
 
-        return new ResponseEntity<>(response, HttpStatus.GATEWAY_TIMEOUT);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    @ExceptionHandler(MyRetailAppTimeoutException.class)
+    public ResponseEntity<MyRetailAppExceptionResponse> timeoutExceptionHandler(MyRetailAppTimeoutException ex) {
+        MyRetailAppExceptionResponse response = new MyRetailAppExceptionResponse();
+        response.setErrorCode("TIMEOUT");
+        response.setErrorMessage(ex.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.GATEWAY_TIMEOUT);
+    }
 }
