d36c5d63b374c01ff878ef44a5b446a2e803e216
Magesh <mageshb@live.com>
2018-07-25 23:13:13 -0500

    @ResponseBody is not required

--- src/main/java/com/myretailapp/controller/ProductController.groovy
+++ src/main/java/com/myretailapp/controller/ProductController.groovy
@@ -14,19 +14,16 @@ class ProductController {
     private ProductService productService
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    @ResponseBody
     ProductInformation productDetails(@PathVariable("id") int id) {
          productService.getProductDetails(id)
     }
 
     @RequestMapping(value = "/{id}/{price}", method = RequestMethod.PUT)
-    @ResponseBody
     void updateProductPriceThroughPath(@PathVariable("id") int id,@PathVariable("price") Double price) {
         productService.updateProductPrice(id,price)
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    @ResponseBody
     void updateProductPriceThroughBody(@RequestBody Price price) {
         productService.updateProductPrice(price)
     }
